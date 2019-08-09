85dc3ea7636f5e77e3ba1c5496622d24ee0d7411
Ramiro Coll Doñetz <coll.ramiro@gmail.com>
2019-08-09 12:20:02 -0300

    Initial project and cocoapods setup

---
+++ RSS-O-Matic/RSS-O-Matic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSS-O-Matic
+//
+//  Created by Ramiro Coll Doñetz on 09/08/2019.
+//  Copyright © 2019 Ramiro Coll Doñetz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
